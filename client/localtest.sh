18f2601e6d7df07bc42147acafb9876ad5f21f36
Abhishek Mishra <ideamonk@gmail.com>
2010-06-05 05:41:12 +0530

    dumb client added, client secret share crappy auth works now, uploads include thumbs and passphrase, history left todo

--- client/localtest.sh
+++ client/localtest.sh
@@ -1,10 +1,11 @@
 import -window root -quality 50 foo.jpg
 convert foo.jpg -quality 50  -thumbnail '160x100' _foo.jpg
-password="R41yCXYTtpvf"
 
-echo "at `date`  `curl -sF img=@foo.jpg -F thumb=@_foo.jpg -F passphrase=$password  http://127.0.0.1:8080/posthere`"
-rm foo*
+password="R41yCXYTtpvf" # passphrase on server
+delay=300 # 5 Minutes
+server="http://127.0.0.1:8080/posthere"
 
-sleep 1
-
-./$0
+echo "at `date`  `curl -sF img=@foo.jpg -F thumb=@_foo.jpg -F passphrase=$password  $server`"
+rm foo.jpg _foo.jpg
+sleep $delay
+./$0 &
