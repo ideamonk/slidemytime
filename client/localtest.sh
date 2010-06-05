3bc6c871b76e03957d73568707c342a438be4c6b
Abhishek Mishra <ideamonk@gmail.com>
2010-06-05 22:05:31 +0530

    added ssl to exemplary local test client

--- client/localtest.sh
+++ client/localtest.sh
@@ -3,7 +3,7 @@ convert foo.jpg -quality 50  -thumbnail '160x100' _foo.jpg
 
 password="x5VpJWYAUte2" # passphrase on server
 delay=300 # 5 Minutes
-server="http://127.0.0.1:8080/posthere"
+server="https://127.0.0.1:8080/posthere"
 
 echo "at `date`  `curl -sF img=@foo.jpg -F thumb=@_foo.jpg -F passphrase=$password  $server`"
 rm foo.jpg _foo.jpg
